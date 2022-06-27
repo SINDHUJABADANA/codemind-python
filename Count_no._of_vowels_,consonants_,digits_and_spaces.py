d3526212fb43c683b5b095664d33712c1a18e4e2
SINDHUJABADANA <97426085+SINDHUJABADANA@users.noreply.github.com>
2022-06-27 22:28:45 +0530

    n=input() v=0 c=0 d=0 s=0 for i in n:     if i in 'aeiou':         v+=1     elif i in ' ':         s+=1     elif i in '0123456789':         d+=1     else:         c+=1 print('Vowels:',v); print('Consonants:',c); print('Digits:',d); print('White spaces:',s);

---
+++ Count_no._of_vowels_,consonants_,digits_and_spaces.py
@@ -0,0 +1,18 @@
+n=input()
+v=0
+c=0
+d=0
+s=0
+for i in n:
+    if i in 'aeiou':
+        v+=1
+    elif i in ' ':
+        s+=1
+    elif i in '0123456789':
+        d+=1
+    else:
+        c+=1
+print('Vowels:',v);
+print('Consonants:',c);
+print('Digits:',d);
+print('White spaces:',s);
