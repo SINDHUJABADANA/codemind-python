2c7176d5671f193ccaa18efe44de8ce8eebb2ca3
SINDHUJABADANA <97426085+SINDHUJABADANA@users.noreply.github.com>
2022-06-28 19:03:58 +0530

    n,m=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) a=set(a) b=set(b) c=0 for i in a:     if i !=' ':         if i  not in b:             c+=1 for i in b:     if i  not in a:         c+=1 print(c)

--- Count_elements_II.py
+++ Count_elements_II.py
@@ -6,9 +6,9 @@ b=set(b)
 c=0
 for i in a:
     if i !=' ':
-        if i not in b:
+        if i  not in b:
             c+=1
 for i in b:
-    if i not in a:
+    if i  not in a:
         c+=1
 print(c)
