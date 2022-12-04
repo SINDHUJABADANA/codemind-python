95d8f7deb6169a56c6b230a08a19355d98c6f29c
SINDHUJABADANA <97426085+SINDHUJABADANA@users.noreply.github.com>
2022-12-04 18:45:21 +0530

    a=int(input()) c=0 rev=0 temp=a*a n=len(str(a)) while(temp):     rem=temp%10     rev=rev*10+rem     c+=1     if c==n:         break     temp=temp//10 rev=str(rev) rev=rev[::-1] if int(rev)==a:     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,15 +1,19 @@
-import math
-def nod(a):
-    c=0
-    while a>0:
-        r=a%10
-        c+=1
-        a//=10
-    return c
 a=int(input())
-n=nod(a)
-asq=a*a
-if asq%math.pow(10,n)==a:
+c=0
+rev=0
+temp=a*a
+n=len(str(a))
+while(temp):
+    rem=temp%10
+    rev=rev*10+rem
+    c+=1
+    if c==n:
+        break
+    temp=temp//10
+rev=str(rev)
+rev=rev[::-1]
+if int(rev)==a:
     print("Automorphic Number")
 else:
     print("Not an Automorphic Number")
+    
