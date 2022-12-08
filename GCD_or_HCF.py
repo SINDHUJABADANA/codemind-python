6bb2a91dc6ca680f4a62bc61bab095ed80252afc
SINDHUJABADANA <97426085+SINDHUJABADANA@users.noreply.github.com>
2022-12-08 20:28:35 +0530

    a,b=map(int,input().split()) for i in range(min(a,b),0,-1):     if a%i==0 and b%i==0:         print(i)         break

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,8 +1,5 @@
-def hcf(a,b):
-    c=b
-    while True:
-        if c%a==0 and c%b==0:
-            return c
-        c+=1
 a,b=map(int,input().split())
-print((a*b)//hcf(a,b))
+for i in range(min(a,b),0,-1):
+    if a%i==0 and b%i==0:
+        print(i)
+        break
